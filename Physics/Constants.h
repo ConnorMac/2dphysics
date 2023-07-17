377acd7b5c5c48e98dc598e62392611457cee0ff
unknown <cmacdougall78@gmail.com>
2023-07-17 16:01:35 +0200

    Added all files

---
+++ Physics/Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 / 60;
+
+const int  PIXELS_PER_METER = 50;
+
+// Global forces
+const float GRAVITY = 9.81;
+
+#endif
